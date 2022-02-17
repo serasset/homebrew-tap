9fbf5aa998bfcd2a8fef02e22797566e1b107a82
serasset <gilles.serasset@imag.fr>
2022-02-17 11:35:00 +0100

    dbnary v3.0.3

---
+++ Formula/dbnary.rb
@@ -0,0 +1,26 @@
+# Generated with JReleaser 1.0.0-M2 at 2022-02-17T11:34:58.247944+01:00
+class Dbnary < Formula
+  desc "Dbnary is an effort to provide multilingual lexical data extracted from wiktionary.
+    The extracted data is made available as LLOD (Linguistic Linked Open Data). This data set has
+    won the Monnet challenge in 2012 and 2014.
+    This project consists in a set of extraction tools (whose status report is given in these maven
+    generated pages. The extracted data is made available on the
+    http://kaiko.getalp.org/about-dbnary/ web site."
+  homepage "http://kaiko.getalp.org/about-dbnary/dbnary-commands"
+  url "https://github.com/serasset/dbnary/releases/download/v3.0.3/dbnary-commands-3.0.3.zip"
+  version "3.0.3"
+  sha256 "42915646e29029f105e43169636cf24ea719139b49a468961fc97f49ece251e9"
+  license "MIT License"
+
+  depends_on "openjdk@11"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/dbnary" => "dbnary"
+  end
+
+  test do
+    output = shell_output("#{bin}/dbnary --version")
+    assert_match "3.0.3", output
+  end
+end
