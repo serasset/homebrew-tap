492bd21614be95fbb66b4ecd29539588682f1699
serasset <gilles.serasset@imag.fr>
2022-10-20 17:04:16 +0000

    dbnary v3.1.3

--- Formula/dbnary.rb
+++ Formula/dbnary.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.2.0 at 2022-10-20T18:28:33.590757+02:00
+# Generated with JReleaser 1.2.0 at 2022-10-20T17:03:57.905302Z
 class Dbnary < Formula
   desc "Dbnary is an effort to provide multilingual lexical data extracted from wiktionary.
     The extracted data is made available as LLOD (Linguistic Linked Open Data). This data set has
@@ -7,9 +7,9 @@ class Dbnary < Formula
     generated pages. The extracted data is made available on the
     http://kaiko.getalp.org/about-dbnary/ web site."
   homepage "http://kaiko.getalp.org/about-dbnary/dbnary-commands"
-  url "https://github.com/serasset/dbnary/releases/download/v3.1.2/dbnary-commands-3.1.2.zip"
-  version "3.1.2"
-  sha256 "4b54df4ae5cfc2a47e75dadbb65c5c386a7d5210c6cbbbddc5f9d1dccb07c687"
+  url "https://github.com/serasset/dbnary/releases/download/v3.1.3/dbnary-commands-3.1.3.zip"
+  version "3.1.3"
+  sha256 "fd3bee1381a55b7cb2eb75e2df7db84cb56bafc71b6dd9d6fd028e0f2cc5104d"
   license "MIT License"
 
   depends_on "openjdk@11"
@@ -21,6 +21,6 @@ class Dbnary < Formula
 
   test do
     output = shell_output("#{bin}/dbnary --version")
-    assert_match "3.1.2", output
+    assert_match "3.1.3", output
   end
 end
