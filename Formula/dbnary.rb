b5d72804dbe20a9904178d6a4a03cb61aa0fdebd
serasset <gilles.serasset@imag.fr>
2024-03-03 16:08:09 +0000

    dbnary v3.1.14

--- Formula/dbnary.rb
+++ Formula/dbnary.rb
@@ -1,15 +1,15 @@
-# Generated with JReleaser 1.2.0 at 2023-11-30T16:26:29.180851Z
+# Generated with JReleaser 1.2.0 at 2024-03-03T16:07:54.475907Z
 class Dbnary < Formula
   desc "Dbnary is an effort to provide multilingual lexical data extracted from wiktionary.
     The extracted data is made available as LLOD (Linguistic Linked Open Data). This data set has
     won the Monnet challenge in 2012 and 2014.
     This project consists in a set of extraction tools (whose status report is given in these maven
     generated pages. The extracted data is made available on the
-    http://kaiko.getalp.org/about-dbnary/ web site."
-  homepage "http://kaiko.getalp.org/about-dbnary/dbnary-commands"
-  url "https://github.com/serasset/dbnary/releases/download/v3.1.13/dbnary-commands-3.1.13.zip"
-  version "3.1.13"
-  sha256 "a8111ba90793d8348291730b7c54703760237af9f14d35add9564a2d70c0a8d2"
+    https://kaiko.getalp.org/about-dbnary/ website."
+  homepage "https://kaiko.getalp.org/about-dbnary/dbnary-commands"
+  url "https://github.com/serasset/dbnary/releases/download/v3.1.14/dbnary-commands-3.1.14.zip"
+  version "3.1.14"
+  sha256 "88e1dd6362f75fb3b4377e0489b14643b4fdf98a5dcfefcdf578f6ca4ed959f5"
   license "MIT License"
 
   depends_on "openjdk@11"
@@ -21,6 +21,6 @@ class Dbnary < Formula
 
   test do
     output = shell_output("#{bin}/dbnary --version")
-    assert_match "3.1.13", output
+    assert_match "3.1.14", output
   end
 end
